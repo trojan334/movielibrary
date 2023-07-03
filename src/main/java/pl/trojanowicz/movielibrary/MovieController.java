d1718c2d77cbb0da2795e65dd71246af80499841
trojan334 <lukasz.trojanowicz@gmail.com>
2023-07-03 15:44:48 +0200

    add controller and repository

---
+++ src/main/java/pl/trojanowicz/movielibrary/MovieController.java
@@ -0,0 +1,25 @@
+package pl.trojanowicz.movielibrary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    //Wstrzykujemy reopo do pobrania metod
+    @Autowired
+    MovieRepository movieRepository;
+
+    @GetMapping("/test")
+    public int test() {
+        return 1;
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAll() {
+        return movieRepository.getAll();
+    }
+}
---
+++ src/main/java/pl/trojanowicz/movielibrary/MovieRepository.java
@@ -0,0 +1,20 @@
+package pl.trojanowicz.movielibrary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    //metoda zwracająca wszystie filmy
+    public List<Movie> getAll(){
+        return jdbcTemplate.query("SELECT id, name, rating FROM movie",
+                BeanPropertyRowMapper.newInstance(Movie.class));
+    }
+}
