cbf08ac9725e1a2844523f66fc35c62700471abd
trojan334 <lukasz.trojanowicz@gmail.com>
2023-07-03 14:16:39 +0200

    class name refactor and add properties

--- src/main/java/pl/trojanowicz/movielibrary/MovieLibraryApplication.java
+++ src/main/java/pl/trojanowicz/movielibrary/MovieLibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovielibraryApplication {
+public class MovieLibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovielibraryApplication.class, args);
+		SpringApplication.run(MovieLibraryApplication.class, args);
 	}
 
 }
